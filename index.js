67ae77de5612b540416b51c63a1a35d1b8b274d7
Ireoluwa <righteousnessakinbola@gmail.com>
2022-10-08 11:52:51 +0100

    made changes to db.js

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-const  createDB = require("./config/db")
-const userRoute = require("./routes/userRoute")
-const notFound = require("./middlewares/notFound")
-require('colors')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express()
+require("colors");
 
-app.use(cors())
+const createDB = require("./config/db");
+const userRoute = require("./routes/userRoute");
+const notFound = require("./middlewares/notFound");
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/auth", userRoute);
 
-dotenv.config()
-const PORT = process.env.PORT
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to my api",
+  });
+});
 
-app.use("/api/v1/auth", userRoute)
+app.use(notFound);
 
-app.use(notFound)
+const PORT = process.env.PORT;
 
-app.get ("/", (req, res) => {
-   res.json({
-   message: "Welcome to my api"
-   })
-})
+const startApp = async () => {
+  try {
+    await createDB();
+    app.listen(PORT, console.log(`server is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+startApp();
 
-const startApp = async ()=>{
- try{ 
-   await createDB()  
-    app.listen(PORT, console.log(`server is running on port ${PORT}`))
-   
- }catch(error){
-  console.log(error)
-  throw error
- }
-
-}
-startApp()
-
-module.exports = app
+module.exports = app;
